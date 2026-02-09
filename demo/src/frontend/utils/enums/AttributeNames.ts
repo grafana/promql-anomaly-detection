fa19f20739566dbd1cb773addd4933c3d1183492
jcreixell <jorge.creixell@grafana.com>
2026-02-09 13:33:22 +0100

    Cleanup demo
      - Remove unnecessary container builds (and delete associated code).
        Instead, upstream images wil be used whenever possible.
      - Fix scraping configuration for otel collector and prometheus
        - Enables exposing /metrics endpoint from otel collector
        - Fix prometheus configuration to allow it to scrape itself
      - Removes redundant dashboards

--- demo/src/frontend/utils/enums/AttributeNames.ts
+++
@@ -1,6 +0,0 @@
-// Copyright The OpenTelemetry Authors
-// SPDX-License-Identifier: Apache-2.0
-
-export enum AttributeNames {
-    SESSION_ID = 'session.id'
-}
--- demo/src/shippingservice/src/shipping_service/tracking.rs
+++
@@ -1,9 +0,0 @@
-// Copyright The OpenTelemetry Authors
-// SPDX-License-Identifier: Apache-2.0
-
-use uuid::Uuid;
-
-/// returns a tracking ID
-pub fn create_tracking_id() -> String {
-    Uuid::new_v4().to_string()
-}
